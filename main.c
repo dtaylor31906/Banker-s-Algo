da509d482c8c2e308cf3a26ebe20ac395f003fb3
dtaylor31906 <dt01812@georgiasouthern.edu>
2015-11-15 16:42:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,12 +11,22 @@ int main ()
 	scanf("%d", processes);
 	printf("Enter the number of resource types");
 	scanf("%d",resourceType);
-	int numResources[resourceType];//array for number of resource for each type
-	int i=0;//itterations
+	int available[resourceType];//array for number of resource for each type
+	int i=0;int j=0;//itterations
+	
 	while(i<resourceType)//fill aray with the user input
 	{
 		printf("Available resource for resource number %d \n", i+1)
-		scanf("%d",&numResources[i]);
+		scanf("%d",&available[i]);
+	}
+	int maxNum[processes][resourceType];//stores the maximum number of resources for each resource
+	for(i=0;i<processes;i++)
+	{
 		
+		for(j=0;j<resourceType;j++)
+		{
+			printf("Enter max number of resource %d for  Process%d",j+1,i+1);
+			scanf("%d",maxNum[i][j]);
+		}
 	}
 }
